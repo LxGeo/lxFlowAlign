a57e96c4713f215f60a73d0a62fcf752b3ada631
sherif-med <cherif.abedrazek@gmail.com>
2022-04-01 07:33:37 +0000

    Adding and using raster register instead of including rio dataset as instance attribute because of pickling issues && updating trainer to use pytorch lightning

--- lxFlowAlign/dataset/ptl/optical_flow_dataset.py
+++ lxFlowAlign/dataset/ptl/optical_flow_dataset.py
@@ -11,42 +11,52 @@ import torch
 from torch.utils.data import Dataset
 from LxGeoPyLibs.vision.image_transformation import Trans_Identity
 
+class RasterRegister(dict):
+
+    def __init__(self):
+        super(RasterRegister, self).__init__()
+    
+    def __del__(self):
+        for k,v in self.items():
+            print("Closing raster at {}".format(k))
+            v.close()
+
+rasters_map=RasterRegister()
 
 class OptFlowRasterDataset(Dataset):
-    
+        
+
     def __init__(self, image1_path=None, image2_path=None, optflow_path=None,
                  augmentation_transforms=None,preprocessing=None, patch_size=(256,256), patch_overlap=(100,100)):
         
+        self.images_path_list=[]
         if os.path.isdir(image1_path):
             in_dir=image1_path
-            self.image1_path = os.path.join(in_dir, "image_1.tif")
-            self.image2_path = os.path.join(in_dir, "image_2.tif")
-            self.optflow_path = os.path.join(in_dir, "flow.tif")
+            self.image1_path = os.path.join(in_dir, "image_1.tif"); self.images_path_list.append(self.image1_path)
+            self.image2_path = os.path.join(in_dir, "image_2.tif"); self.images_path_list.append(self.image2_path)
+            self.optflow_path = os.path.join(in_dir, "flow.tif"); self.images_path_list.append(self.optflow_path)
         
-        assert all([os.path.isfile(f) for f in (self.image1_path, self.image2_path, self.optflow_path)]), "One of optFlow rasters are missing!"
+        assert all([os.path.isfile(f) for f in self.images_path_list]), "One of optFlow rasters are missing!"
         
         if augmentation_transforms is None:
             self.augmentation_transforms=[Trans_Identity()]
         else:
             self.augmentation_transforms = augmentation_transforms
         
-        self.rasters_map={
-            "image_1": rio.open(self.image1_path),
-            "image_2": rio.open(self.image2_path),
-            "flow": rio.open(self.optflow_path)
-            }
+        rasters_map.update({
+            self.image1_path: rio.open(self.image1_path),
+            self.image2_path: rio.open(self.image2_path),
+            self.optflow_path: rio.open(self.optflow_path)
+            })
         
-        raster_shapes = [dst.shape for dst in self.rasters_map.values()]
+        raster_shapes = [dst.shape for dst in [rasters_map[path] for path in self.images_path_list]]
         assert len(set(raster_shapes)) == 1, "Rasters have different shapes!" 
         
         self.Y_size, self.X_size = raster_shapes[0]                
         self.preprocessing=preprocessing
+        self.is_setup=False
         self.setup(patch_size, patch_overlap)
-            
-    
-    def __del__(self):        
-        for v in self.rasters_map.values():
-            v.close()
+                
     
     def setup(self, patch_size, patch_overlap):
         """
@@ -77,9 +87,9 @@ class OptFlowRasterDataset(Dataset):
         window_y_start = self.window_y_starts[window_idx%len(self.window_y_starts)]
         c_window = rio.windows.Window(window_x_start, window_y_start, *self.patch_size )
         
-        img1 = self.rasters_map["image_1"].read(window=c_window) 
-        img2 = self.rasters_map["image_2"].read(window=c_window)
-        flow = self.rasters_map["flow"].read(window=c_window) 
+        img1 = rasters_map[self.image1_path].read(window=c_window) 
+        img2 = rasters_map[self.image2_path].read(window=c_window)
+        flow = rasters_map[self.optflow_path].read(window=c_window) 
         
         c_trans = self.augmentation_transforms[transform_idx]
         img1, flow = c_trans(img1, flow)
@@ -89,8 +99,8 @@ class OptFlowRasterDataset(Dataset):
             img1 = self.preprocessing(img1)
             img2 = self.preprocessing(img2)
         
-        img1 = torch.from_numpy(img1).float()
-        img2 = torch.from_numpy(img2).float()
+        img1 = torch.from_numpy(img1).float() * 255
+        img2 = torch.from_numpy(img2).float() * 255
         flow = torch.from_numpy(flow).float()
         
         # valid_mask to flow
--- lxFlowAlign/training/launch_training.py
+++ lxFlowAlign/training/launch_training.py
@@ -9,12 +9,17 @@ import os
 import click
 import torch
 from lxFlowAlign.training.ptl.optflow_model import lightningOptFlowModel
+from lxFlowAlign.training.ptl.trainer_parameter_loader import load_cfg_trainer_params
 import ezflow
 from ezflow.engine import get_training_cfg
 from ezflow.models import get_default_model_cfg
-from ezflow.engine import Trainer
+from ezflow.data.dataloader.device_dataloader import DeviceDataLoader
 from LxGeoPyLibs.dataset.multi_dataset import MultiDatasets
 from lxFlowAlign.dataset.ptl.optical_flow_dataset import OptFlowRasterDataset
+
+import pytorch_lightning as pl
+
+
 
 
 @click.command()
@@ -23,40 +28,44 @@ from lxFlowAlign.dataset.ptl.optical_flow_dataset import OptFlowRasterDataset
 @click.argument('val_data_dir', type=click.Path(exists=True))
 @click.argument('ckpt_dir', type=click.Path(exists=False))
 @click.argument('log_dir', type=click.Path(exists=False))
-@click.option('--custom_model_cfg', required=False, type=click.Path(exists=True))
-@click.option('--custom_training_cfg', required=False, type=click.Path(exists=True))
+@click.option('--custom_model_cfg', required=True, type=click.Path(exists=True))
+@click.option('--custom_training_cfg', required=True, type=click.Path(exists=True))
 def main(arch, train_data_dir, val_data_dir, ckpt_dir, log_dir, custom_model_cfg, custom_training_cfg):
     
         
-    model_cfg = get_default_model_cfg(arch) if not custom_training_cfg \
-        else ezflow.config.get_cfg(cfg_path=custom_model_cfg, custom=True)
-    model_cfg.ENCODER.FEATURE.IN_CHANNELS=1
-    model_cfg.ENCODER.CONTEXT.IN_CHANNELS=1
+    model_cfg = ezflow.config.get_cfg(cfg_path=custom_model_cfg, custom=True)
+    
     light_model = lightningOptFlowModel(arch, model_cfg)
     
-    training_cfg = get_training_cfg(cfg_path="raft_default.yaml", custom=False) if not custom_training_cfg \
-        else get_training_cfg(cfg_path=custom_training_cfg, custom=True)
-    
-    training_cfg.CKPT_DIR = ckpt_dir
-    training_cfg.LOG_DIR = log_dir
-    
+    training_cfg = get_training_cfg(cfg_path=custom_training_cfg, custom=True)
+
+    training_cfg["MODEL_CHECKPOINT_CALLBACK"]["PARAMS"]["dirpath"] = ckpt_dir
+    training_cfg["TENSORBOARD_LOGGER"]["LOG_PATH"] = log_dir
+
+    training_params = load_cfg_trainer_params(training_cfg)    
+        
     ## data loaders
     train_dataset = MultiDatasets( (OptFlowRasterDataset(
         os.path.join(train_data_dir, sub_folder)) for sub_folder in os.listdir(train_data_dir)))
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=training_cfg.DATA.BATCH_SIZE, num_workers=0, shuffle=True)
-    
+    train_dataloader = train_dataloader
+
     valid_dataset = MultiDatasets( (OptFlowRasterDataset(
         os.path.join(val_data_dir, sub_folder)) for sub_folder in os.listdir(val_data_dir)))    
     valid_dataloader = torch.utils.data.DataLoader(valid_dataset, batch_size=training_cfg.DATA.BATCH_SIZE, num_workers=0, shuffle=True)
-    
-    trainer = Trainer(
-        cfg=training_cfg,
-        model=light_model,
-        train_loader=train_dataloader,
-        val_loader=valid_dataloader
-    )
-    
-    trainer.train(n_epochs=10)
+    valid_dataloader = valid_dataloader
+
+    light_model = light_model.cuda()
+    light_model.sample_val = next(iter(valid_dataloader))
+
+    trainer = pl.Trainer(**training_params)
+
+    if (training_params["auto_scale_batch_size"] or training_params["auto_lr_find"]):
+        trainer.tune(light_model, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader)
+    else:
+        trainer.fit(light_model, train_dataloaders=train_dataloader, val_dataloaders=valid_dataloader)
+
+
     
 
 
