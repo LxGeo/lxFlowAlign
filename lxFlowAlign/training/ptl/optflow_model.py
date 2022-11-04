82453c7916a12c489764e26bb314d1695884a4cc
sherif-med <cherif.abedrazek@gmail.com>
2022-11-04 15:56:57 +0100

    fixing save_hyperparameters && adding quiver plot to view flow

--- lxFlowAlign/training/ptl/optflow_model.py
+++ lxFlowAlign/training/ptl/optflow_model.py
@@ -7,8 +7,7 @@ Created on Wed Mar 16 11:49:21 2022
 
 from matplotlib import pyplot as plt
 import torch
-import numpy as np
-import os
+from LxGeoPyLibs.vision.plot.flow_plot import plot_quiver
 
 import pytorch_lightning as pl
 from ezflow.models import build_model
@@ -22,9 +21,10 @@ class lightningOptFlowModel(pl.LightningModule):
     def __init__(self, arch, cfg, **kwargs):
         
         super(lightningOptFlowModel, self).__init__()
-        
-        self.arch=arch
-        self.cfg = cfg
+        self.save_hyperparameters()
+
+        self.arch=self.hparams.arch
+        self.cfg = self.hparams.cfg
         self.in_channels = 3# self.cfg.ENCODER.FEATURE.IN_CHANNELS or self.cfg.ENCODER.IN_CHANNELS
         
         self.model = build_model(self.arch, cfg=self.cfg)
@@ -35,8 +35,6 @@ class lightningOptFlowModel(pl.LightningModule):
             self.loss_fn = FUNCTIONAL_REGISTRY.get(self.cfg.CRITERION.NAME)(**criterion_params)
         else:
             self.loss_fn = loss_registry.get(self.cfg.CRITERION.NAME)()
-                
-        self.save_hyperparameters()
         
     
     def forward(self, image_pair):
@@ -114,13 +112,31 @@ class lightningOptFlowModel(pl.LightningModule):
             self.logger.experiment.add_image("flow_gt_{}".format(i),norm(gt_flow[i,:2]),self.current_epoch)
             self.logger.experiment.add_image("flow_preds_{}".format(i),norm(preds[i,:2]),self.current_epoch)
     
+    def image_flow_grid(self, image_pair, gt_flow, preds):
+        
+        def norm(x):
+            return (x-x.min()) / (x.max()-x.min())
+        
+        bs = self.sample_val[1].shape[0]
+        img1, img2 = image_pair
+
+        preds=preds.permute(0,2,3,1)
+        gt_flow=gt_flow.permute(0,2,3,1)
+
+        for i in range(bs):
+            self.logger.experiment.add_image("img1_{}".format(i),norm(img1[i]),self.current_epoch)
+            self.logger.experiment.add_image("img2_{}".format(i),norm(img2[i]),self.current_epoch)
+            
+            self.logger.experiment.add_figure("flow_gt_{}".format(i),plot_quiver(gt_flow[i], spacing=10, scale=1, color="#ff44ff"),self.current_epoch)
+            self.logger.experiment.add_figure("flow_preds_{}".format(i),plot_quiver(preds[i].cpu(), spacing=10, scale=1, color="#ff44ff"),self.current_epoch)
+
     def validation_epoch_end(self, outputs):
         
         if hasattr(self, "sample_val"):
             image_pair,gt_flow = self.sample_val
             image_pair = [im.to(self.device) for im in image_pair]
             logits = self.forward(image_pair)            
-            self.image_grid(image_pair, gt_flow, logits)
+            self.image_flow_grid(image_pair, gt_flow, logits)
     
     
     
