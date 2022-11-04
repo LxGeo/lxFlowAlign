bd0e642b42e483e011ab7e001ff53a9f7dcad13a
sherif-med <cherif.abedrazek@gmail.com>
2022-11-04 15:59:19 +0100

    refactor dhm2flow by adding reverse transformation option and using rasterDataset to take into account large images

--- lxFlowAlign/utils/dhm2flow.py
+++ lxFlowAlign/utils/dhm2flow.py
@@ -1,47 +1,77 @@
 
 import click
-import os
 import numpy as np
 import rasterio as rio
 from LxGeoPyLibs.satellites.imd import IMetaData
 from LxGeoPyLibs.satellites import formulas
+from LxGeoPyLibs.dataset.raster_dataset import RasterDataset
+from LxGeoPyLibs.dataset.patchified_dataset import CallableModel
+from math import radians
 
+
+class DhmFlowModel(CallableModel):
+
+    device = "cpu"
+
+    def __init__(self, x_flow, y_flow, x_res, y_res, dhm2flow=True):
+        super().__init__()
+        self.x_flow = x_flow
+        self.y_flow = y_flow
+        self.x_res = x_res
+        self.y_res = y_res
+        self.dhm2flow = dhm2flow
+
+    def forward(self, batch_image):
+        batch_image = batch_image[0]
+        batched_pred = []
+        for idx in range(batch_image.shape[0]):
+            batched_pred.append( self.forward_one(batch_image[idx]) )
+        return np.stack(batched_pred, axis=0)
+        
+    
+    def forward_one(self, image):
+        if self.dhm2flow:
+            assert image.shape[0]==1, f"Input image of shape {image.shape} has {image.shape[0]} bands when expected 1!"
+            out_image = np.concatenate( [image*self.x_flow/self.x_res, image*self.y_flow/self.y_res ] )
+            return out_image
+        else:
+            assert image.shape[0]==2, f"Input image of shape {image.shape} has {image.shape[0]} bands when expected 2!"
+            #out_image = image[0]*self.x_res/self.x_flow + image[1]*self.y_res/self.y_flow
+            out_image = self.x_res * image[0]/self.x_flow + self.y_res * image[1]/self.y_flow
+            out_image = np.expand_dims(out_image, 0)
+            return out_image
+        
+        
+    
 
 @click.command()
-@click.argument('input_dhm_path', type=click.Path(exists=True))
-@click.option('--im1_imd', type=click.Path(exists=True))
-@click.option("--im2_imd", type=click.Path(exists=True))
-@click.option("--out_flow", type=click.Path(exists=False))
-def main(input_dhm_path, im1_imd, im2_imd, out_flow):
+@click.option("-i", "--input_image", type=click.Path(exists=True))
+@click.option('--imd1', type=click.Path(exists=True))
+@click.option("--imd2", type=click.Path(exists=True))
+@click.option("-o", "--out_image", type=click.Path(exists=False))
+@click.option("-inv","--inverse", is_flag=True, help="Flag for inverse transformation (flow2dhm)!")
+def main(input_image, imd1, imd2, out_image, inverse):
     """
     """
 
-    with rio.open(input_dhm_path) as in_dst:
+    with rio.open(input_image) as in_dst:
         out_profile = in_dst.profile.copy()
         x_resolution = out_profile["transform"][0]
         y_resolution = out_profile["transform"][4]
-        img_array = in_dst.read()
 
-    imd1 = IMetaData(im1_imd)
-    imd2 = IMetaData(im2_imd)
+    imd1 = IMetaData(imd1)
+    imd2 = IMetaData(imd2)
 
-    x_flow, y_flow = formulas.compute_roof2roof_constants(imd1.satAzimuth(), imd1.satElevation(), imd2.satAzimuth(), imd2.satElevation())
+    x_flow, y_flow = formulas.compute_roof2roof_constants(radians(imd1.satAzimuth()), radians(imd1.satElevation()),
+     radians(imd2.satAzimuth()), radians(imd2.satElevation())
+     )
 
-    out_image = np.concatenate( [img_array*x_flow/x_resolution, img_array*y_flow/y_resolution ] )
+    transformer_model = DhmFlowModel(x_flow, y_flow, x_resolution, y_resolution, not inverse)
 
-    out_image[out_image>100]=0
-    out_image[out_image<-100]=0
+    in_dataset = RasterDataset(input_image)
+
+    in_dataset.predict_to_file(out_image, transformer_model, tile_size=(256,256))
     
-    ## check output folder
-    out_folder = os.path.dirname(out_flow)
-    if not os.path.isdir(out_folder):
-        os.makedirs(out_folder)
-    
-    ## Update profile before saving
-    out_profile["count"] = 2
-
-    with rio.open(out_flow, "w", **out_profile) as out_dst:
-        out_dst.write(out_image)
     
 
 if __name__ == "__main__":
